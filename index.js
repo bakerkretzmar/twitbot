40c6155e66b18c30e0ad5875e69e9491418eb28b
bakerkretzmar <jacobtbk@gmail.com>
2016-10-09 00:33:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 // START HEROKU SETUP
 var express = require("express");
 var app = express();
-app.get('/', function(req, res){ res.send('The robot is happily running.'); });
+app.get('/', function(req, res) {
+  res.send('The robot is anxiously running.');
+});
 app.listen(process.env.PORT || 5000);
 // END HEROKU SETUP
 
+// LISTBOT
 
-// Listbot config
-//
 // Config.keys uses environment variables so sensitive info is not in the repo.
 var config = {
-    me: 'itmepinkbathrom', // The authorized account with a list to retweet.
-    myList: 'beef', // The list we want to retweet.
-    regexFilter: '(elinabloom||allieegraham)', // Accept only tweets matching this regex pattern.
-    regexReject: 'RT', // AND reject any tweets matching this regex pattern.
+  me: 'itmepinkbathrom', // The authorized account with a list to retweet.
+  myList: 'beef', // The list we want to retweet.
+  regexFilter: '(elinabloom||allieegraham)', // Accept only tweets matching this regex pattern.
+  regexReject: 'RT', // AND reject any tweets matching this regex pattern.
 
-    keys: {
-        consumer_key: process.env.TWITTER_CONSUMER_KEY,
-        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-        access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-    },
+  keys: {
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  }
 };
-
 
 // Get the members of our list, and pass them into a callback function.
 function getListMembers(callback) {
